7d26597c9ee5ae6fb638614a9631350165f6eb09
Kyle <notxap55@yahoo.com>
2024-10-24 16:18:19 -0700

    updating check_answer to QuizBrain

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -14,8 +17,9 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250)
         self.question_text = self.canvas.create_text(
-            300//2,
-            250//2,
+            150,
+            125,
+            width=280,
             text='Question Text',
             font=('Arial', 20, 'italic')
         )
@@ -28,4 +32,10 @@ class QuizInterface:
         self.false_button = Button(image=self.false_image, borderwidth=0, highlightthickness=0, bg=THEME_COLOR)
         self.false_button.grid(row=2, column=1, pady=(20, 0))
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
